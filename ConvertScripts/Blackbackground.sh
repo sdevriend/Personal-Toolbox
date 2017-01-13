e3b0c555b07ca13c6743d40e1792058a3b36fbb3
Sebastiaan de Vriend <sebastiaandevriend@gmail.com>
2017-01-13 10:37:27 +0100

    Update Blackbackground.sh

--- ConvertScripts/Blackbackground.sh
+++ ConvertScripts/Blackbackground.sh
@@ -6,3 +6,26 @@ convert ${filename} -fuzz 30% -fill Red -opaque 'rgb(0, 0, 0)
 ' - | convert - -crop 3600x3632+0+0 - | convert - -crop 0x0+900+0 - | convert - -fuzz 10% -fill Black -opaque 'rgb(255,
 0, 0)' ${round}/CONVERT_${picturefilename}
 done
+
+#CONVERT ALL DATA
+#egrep to filter out earlier converts!
+for filename in $(ls | egrep -v "CONVERT")
+do
+convert ${filename} -fuzz 30% -fill Black -opaque 'rgb(0, 0, 0)' - |
+convert - -crop 3600x2848+0+0 - | convert - -crop 0x0+900+0  CONVERT_${filename}  
+done
+beep
+#CONVERT SINGLE
+for filename in "DSC_2133EDITED_NEW-SCALE.JPG" 
+do
+convert ${filename} -fuzz 25% -fill Black -opaque 'rgb(0, 0, 0)' - |
+convert - -crop 3500x2848+0+0 - | convert - -crop 0x0+900+0  CONVERT_${filename}  
+done
+#CONVERT RED SINGLE
+for filename in "DSC_2133EDITED_NEW-SCALE.JPG"  
+do
+convert ${filename} -fuzz 25% -fill Black -opaque 'rgb(0, 0, 0)' - |
+convert - -crop 3500x2848+0+0 - | convert - -crop 0x0+900+0 - |
+convert - -fuzz 10% -fill Black -opaque 'rgb(147, 25, 25)' SCALECONVERT_NEW_${filename} 
+done
+beep
